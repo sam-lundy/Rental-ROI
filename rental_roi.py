848376e3a5f8434f3f8b4fdcd0d64664a7989fc2
Sam Lundy <samuel.lundy@gmail.com>
2023-08-04 21:41:45 -0600

    initial commit

---
+++ rental_roi.py
@@ -0,0 +1,73 @@
+from rental_art import logo
+
+class CashOnCash:
+    def __init__(self):
+        pass
+
+
+
+
+
+#step 1: Income method
+
+    def income(self):
+        pass
+
+
+
+
+
+
+
+#step 2: Expenses method
+
+    def expenses(self):
+        pass
+
+
+
+
+
+
+
+
+#step 3: Cash flow method
+
+    def cash_flow(self):
+        pass
+
+
+
+
+
+
+
+#step 4: Cash on Cash ROI
+
+    def roi_calc(self):
+        pass
+
+
+
+
+#rental_roi method
+
+    def rental_roi(self):
+        print(logo)
+
+
+
+
+
+
+
+#main function
+
+def main():
+    calc_roi = CashOnCash()
+    calc_roi.rental_roi()
+
+
+
+if __name__ == "__main__":
+    main()
