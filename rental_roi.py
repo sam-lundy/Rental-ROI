ae8289657337d3302b4f5b550e2c6a4b68418702
Sam Lundy <samuel.lundy@gmail.com>
2023-08-04 23:08:49 -0600

    added income method

--- rental_roi.py
+++ rental_roi.py
@@ -2,7 +2,9 @@ from rental_art import logo
 
 class CashOnCash:
     def __init__(self):
-        pass
+        self.income_types = {}
+        self.total_income = 0
+        
 
 
 
@@ -11,11 +13,38 @@ class CashOnCash:
 #step 1: Income method
 
     def income(self):
-        pass
 
+        while True:
+            try:
+                print(self.income_types)
+                print(self.total_income)
+                #Rental income
+                rent_income = int(input("Enter the rental income amount or enter '000' to stop: "))
+                if rent_income > 1 and rent_income < 999999999:
+                    self.income_types.update({"Rent": rent_income})
+                    self.total_income += rent_income
+                elif rent_income == 000:
+                    break
+                else:
+                    print("\nPlease enter a valid rental income amount.\n")
+                    continue
 
+                #Additional Income
+                other_income = int(input("\nEnter any other additional income. (Laundry, Storage, etc): "))
+                if other_income > 1 and other_income < 999999999:
+                    self.income_types.update({"Other": other_income})
+                    self.total_income += other_income
+                else:
+                    print("\nPlease enter a valid additional income amount.\n")
 
+                done_adding_income = input("Add more income? (type 'y' or 'n'): ")
+                if done_adding_income == 'n':
+                    break
+                else:
+                    continue
 
+            except ValueError:
+                print("Enter a number!")
 
 
 
@@ -54,7 +83,40 @@ class CashOnCash:
 
     def rental_roi(self):
         print(logo)
+        print("Welcome to the Rental Property Return on Investment (RoI) Calculator.")
+        print("This calculator calculates RoI based on Income, Expenses, and Cash Flow.")
+        while True:
+            menu_selection = input("""Please choose from the following menus: \n
+                1. Income
+                2. Expenses
+                3. Cash Flow
+                4. RoI Calculation (Cash on Cash)
+                5. Reset Values
+                6. Quit \n""")
+            
+            try:
 
+                menu_selection = int(menu_selection)
+
+                if 1 <= menu_selection <= 6:
+
+                    if menu_selection == 1:
+                        self.income()
+                    elif menu_selection == 2:
+                        self.expenses()
+                    elif menu_selection == 3:
+                        self.cash_flow()
+                    elif menu_selection == 4:
+                        self.roi_calc()
+                    elif menu_selection == 5:
+                        print("Reset values")
+                    elif menu_selection == 6:
+                        print("\nThank you for using the Rental Income Calculator.")
+                        break
+                else:
+                    print("\nInvalid choice. Please enter a number from 1 to 6.\n")
+            except ValueError:
+                print("\nInvalid input. Please enter a valid number jackass.\n")
 
 
 
